1e4675611582a1ccbd4e8def966649b5846b49a3
Lachlan Alsop <t833909@spark.co.nz>
2024-11-06 14:57:00 +1300

    working main, need to wrap it in a tui now

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use rust_http::{echo_server::setup_server, http::{HttpHeader, HttpMethod, HttpRequest, HttpResponse}, response_codes::ResponseCode};
+use std::io::Error;
 
-fn main() -> Result<()> {
+use rust_http::{client::HttpClient, echo_server::setup_server, http::{HttpMethod, HttpRequest}};
+
+fn main() -> Result<(), Error> {
     let server_addr ="127.0.0.1:8004".to_string();
+    setup_server(&server_addr)?;
+
     let client_addr = "127.0.0.1:8005".to_string();
-    let socket = TcpPort::new(&client_addr).expect("couldn't bind to client address");
+    let client = HttpClient::new(&client_addr)?;
     
     let req = HttpRequest {
         method: HttpMethod::Get,
@@ -12,32 +16,8 @@ fn main() -> Result<()> {
         body: "Hello, ".to_string()
     };
 
-    let pwost = HttpRequest {
-        method: HttpMethod::Post,
-        endpoint: "notice this doesn't do jack yet".to_string(),
-        headers: vec![HttpHeader{ key: "host".to_string(), value: "localhost:8000".to_string() }],
-        body: "Hello, ".to_string()
-    };
-
-    assert!(req == HttpRequest::from_string(req.to_string()).unwrap());
-
-    let res = HttpResponse {
-        status_code: ResponseCode::OK,
-        headers: vec![],
-        body: "World!".to_string(),
-    };
-    assert!(res == HttpResponse::from_string(res.to_string()).unwrap());
-
-    setup_server(&server_addr)?;
-
-    socket.send(req.to_string(), &server_addr)?;
-    let (response, _) = socket.recieve()?;
-    println!("{:.?}", HttpResponse::from_string(response)?);
-
-
-    socket.send(pwost.to_string(), &server_addr)?;
-    let (response, _) = socket.recieve()?;
-    println!("{:.?}", HttpResponse::from_string(response)?);
+    let res = client.send(req, &server_addr)?;
+    println!("{:.?}", res);
 
     Ok(())
 }
