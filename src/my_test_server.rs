31e3fb52294c677e17219603e717818bddc28d3d
DoradoAcero <lachlan.alsop@gmail.com>
2024-11-09 07:50:43 +1300

    adding in another echo endpoint to my server

--- src/my_test_server.rs
+++ src/my_test_server.rs
@@ -1,6 +1,6 @@
 use std::io::Error;
 
-use rust_http::{endpoints::get, http::{HttpRequest, HttpResponse}, response_codes::ResponseCode, server::Router};
+use rust_http::{endpoints::{get, post}, http::{HttpRequest, HttpResponse}, response_codes::ResponseCode, server::Router};
 
 
 // this is really stupid, but itsa first pass, ill get something running and during getting it going ill find a way to make it more ergonomic
@@ -14,11 +14,20 @@ fn process(_: HttpRequest) -> HttpResponse {
     }
 }
 
+fn echo(req: HttpRequest) -> HttpResponse {
+    HttpResponse { 
+        status_code: ResponseCode::OK,
+        headers: req.headers.clone(),
+        body: format!("{:.?}", req),
+    }
+}
+
 
 pub fn setup_my_server(server_addr: &String) -> Result<(), Error> {
     let router = Router::new(&server_addr)?
-        .add_endpoint("/".to_string(), get(process));
+        .add_endpoint("/".to_string(), get(process))
+        .add_endpoint("/echo".to_string(), post(echo));
 
-    router.server_loop()?;
+    router.server_loop()?; // goes off and spawns a thread running the server in the backgrond
     Ok(())
 }
