d8b7adb03d7e74035aeaf569cd7c5522f5d0f896
DoradoAcero <lachlan.alsop@gmail.com>
2024-11-09 07:45:38 +1300

    made changes to the http lib, made corresponding changes to the test server

--- src/my_test_server.rs
+++ src/my_test_server.rs
@@ -1,39 +1,24 @@
 use std::io::Error;
 
-use rust_http::{http::{HttpMethod, HttpRequest, HttpResponse}, response_codes::ResponseCode, server::{Endpoint, Router}};
+use rust_http::{endpoints::get, http::{HttpRequest, HttpResponse}, response_codes::ResponseCode, server::Router};
 
 
 // this is really stupid, but itsa first pass, ill get something running and during getting it going ill find a way to make it more ergonomic
 // https://github.com/tokio-rs/axum, I want to aim for a axum like syntax, or at least something relatively close
 // I honestly am probably going to stay away from types, and make it fn pointers, and you input the endpoint/method in the add to router fn
-struct FirstEndpoint {}
-
-impl FirstEndpoint {
-    pub fn new() -> FirstEndpoint {
-        FirstEndpoint {}
+fn process(_: HttpRequest) -> HttpResponse {
+    HttpResponse {
+        status_code: ResponseCode::OK,
+        headers: vec![],
+        body: "{\"Hello,\": \" World!\"}".to_string(),
     }
 }
 
-impl Endpoint for FirstEndpoint {
-    fn process(&self, _: HttpRequest) -> HttpResponse {
-        HttpResponse {
-            status_code: ResponseCode::OK,
-            headers: vec![],
-            body: "{\"Hello,\": \" World!\"}".to_string(),
-        }
-    }
-
-    fn get_endpoint_type(&self) -> HttpMethod {
-        HttpMethod::Get
-    }
-}
 
 pub fn setup_my_server(server_addr: &String) -> Result<(), Error> {
-    let mut router = Router::new(&server_addr)?;
+    let router = Router::new(&server_addr)?
+        .add_endpoint("/".to_string(), get(process));
 
-    let endpoint = Box::new(FirstEndpoint::new());
-    router.add_endpoint("/".to_string(), endpoint);
-    
     router.server_loop()?;
     Ok(())
 }
