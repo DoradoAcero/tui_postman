ebc776463c8691f022407e653e59794d8032f5ac
DoradoAcero <lachlan.alsop@gmail.com>
2024-11-09 21:52:10 +1300

    removing old comments

--- src/my_test_server.rs
+++ src/my_test_server.rs
@@ -3,9 +3,6 @@ use std::io::Error;
 use rust_http::{endpoints::{get, post}, http::{HttpRequest, HttpResponse}, response_codes::ResponseCode, server::Router};
 
 
-// this is really stupid, but itsa first pass, ill get something running and during getting it going ill find a way to make it more ergonomic
-// https://github.com/tokio-rs/axum, I want to aim for a axum like syntax, or at least something relatively close
-// I honestly am probably going to stay away from types, and make it fn pointers, and you input the endpoint/method in the add to router fn
 fn process(_: HttpRequest) -> HttpResponse {
     HttpResponse {
         status_code: ResponseCode::OK,
